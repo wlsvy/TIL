7b446bf760a26978374ebd56dcbacfadb8b08dd0
wlsvy <hubjpkim@gmail.com>
2020-05-31 20:44:25 +0900

    Update Effective Modern C++ Item09

--- Effective Modern C++/EffectiveModernCpp/main.cpp
+++ Effective Modern C++/EffectiveModernCpp/main.cpp
@@ -5,6 +5,7 @@
 
 #include "Item07.h"
 #include "Item08.h"
+#include "Item09.h"
 
 using namespace std;
 
@@ -14,13 +15,14 @@ int main() {
 
 	//Item01::ParamType_Function::RunSample();
 	//Item07::OtherCase::RunSample();
-	Item08::RunSample();
+	Item09::RunSample();
 
 	system("pause");
 	return 0;
 }
 
 /*
+
 #pragma once
 #include <iostream>
 #include <vector>
@@ -33,4 +35,19 @@ using namespace std;
 
 		}
 	}
+*/
+
+/*
+	class Widget {
+public:
+	Widget() { cout << "default ctor" << endl; }
+	Widget(const Widget& rhs) { cout << "copy ctor" << endl; }
+	Widget(Widget&& rhs) { cout << "move ctor" << endl; }
+	~Widget() {cout<<"dtor" << endl;}
+
+	Widget& operator=(const Widget& rhs) { *this = rhs; return *this; }
+
+	//operator int() const { return 0; }
+	//operator float() const { return 0.0f; }
+};
 */
