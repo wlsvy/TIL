52d972b69be59fbe4663ba8928171bdf6adc29d5
Jinpyo Kim <hubjpkim@gmail.com>
2020-06-13 16:55:48 +0900

    Create TypeAliasing.cpp

---
+++ Coding_Problem_Solving/Helper/TypeAliasing.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+
+using UINT = unsigned int;
+using ULONG = unsigned long;
+using LL = long long;
+using ULL = unsigned long long;
+
+#define FORI(n) for(int i = 0; i < (n); i++)
+#define FORJ(n) for(int j = 0; j < (n); j++)
+#define FORK(n) for(int k = 0; k < (n); k++)
+#define FORIR(n) for(int i = (n) - 1; i >= 0; i--)
+#define FORJR(n) for(int j = (n) - 1; j >= 0; j--)
+#define FORKR(n) for(int k = (n) - 1; k >= 0; k--)
+
+using VB = std::vector<bool>;
+using VVB = std::vector<std::vector<bool>>;
+using VI = std::vector<int>;
+using VVI = std::vector<std::vector<int>>;
+using VS = std::vector<std::string>;
+using VVS = std::vector<std::vector<std::string>>;
+using UMII = std::unordered_map<int, int>;
+using UMIVI = std::unordered_map<int, std::vector<int>>;
+using MII = std::map<int, int>;
+using MIVI = std::map<int, std::vector<int>>;
+using PII = std::pair<int, int>;
+using PIVI = std::pair<int, std::vector<int>>;
