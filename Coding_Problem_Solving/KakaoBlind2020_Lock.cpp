dd6882d6fb316a2e1e63e3dc80cdd84272e4a158
Jinpyo Kim <hubjpkim@gmail.com>
2020-06-14 11:15:05 +0900

    Create KakaoBlind2020_Lock.cpp

---
+++ Coding_Problem_Solving/KakaoBlind2020_Lock.cpp
@@ -0,0 +1,67 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+using VVI = std::vector<std::vector<int>>;
+
+VVI Lock;
+int keySize, lockSize;
+
+bool isMatrixValid(VVI& key, int by, int bx) {
+
+	auto lockM = Lock;
+	for (int i = 0; i < keySize; i++) {
+		for (int j = 0; j < keySize; j++) {
+			int cy = i + by;
+			int cx = j + bx;
+			if (cy < 0 || cy >= lockSize || cx < 0 || cx >= lockSize) continue;;
+			lockM[cy][cx] += key[i][j];
+		}
+	}
+
+
+	for (int i = 0; i < lockSize; i++) {
+		for (int j = 0; j < lockSize; j++) {
+			if (lockM[i][j] != 1) return false;
+		}
+	}
+
+	return true;
+}
+
+
+void rotateKey(VVI &key) {
+
+	VVI tmp(keySize, vector<int>(keySize));
+
+	for (int j = keySize - 1; j >= 0; j--) {
+		for (int i = 0; i < keySize; i++) {
+			tmp[i][j] = key[keySize - j - 1][i];
+		}
+	}
+
+	key = tmp;
+}
+
+bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
+	Lock = lock;
+	keySize = key.size();
+	lockSize = lock.size();
+
+	for (int r = 0; r < 4; r++) {
+		for (int i = -keySize; i <= keySize + lockSize; i++) {
+			for (int j = -keySize; j <= keySize + lockSize; j++) {
+				if (isMatrixValid(key, i, j)) return true;
+			}
+		}
+		rotateKey(key);
+	}
+
+	return false;
+}
