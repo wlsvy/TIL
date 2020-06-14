da7144ad77fcc32790a2ecbcba751ab0cb646ea8
Jinpyo Kim <hubjpkim@gmail.com>
2020-06-14 11:53:26 +0900

    Update KakaoBlind2020_Lock.cpp

--- Coding_Problem_Solving/KakaoBlind2020_Lock.cpp
+++ Coding_Problem_Solving/KakaoBlind2020_Lock.cpp
@@ -1,3 +1,10 @@
+/*
+실수
+1. key와 lock 이 유효한지 판정하는 코드를 작성할 때 비효율적을 작성. 
+(lock 배열 값복사를 하면 간단한데, 그러지 않고, if 문 여러개로 분기를 나눠서 시간을 지체)
+2. 맨 처음 matrix rotate 함수를 구현할 때, 완전히 잘못 구현. 90도 회전이 아니라, 한칸씩 시계방향으로 움직이도록 구현해서 시간 지체
+*/
+
 #include <string>
 #include <vector>
 #include <algorithm>
