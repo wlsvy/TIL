dd775bb3b7bd32ca2a8115d947c6338d072f8a88
wlsvy <hubjpkim@gmail.com>
2020-06-29 23:45:41 +0900

    Update Effective C++ Item01

---
+++ Effective C++/EffectiveCPP/EffectiveCPP/EffectiveC++/Item01.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <array>
+
+//C++를 언어들의 집합체로 바라보는 안목은 필수
+namespace Item01 {
+	/*
+		C++을 사용한 효과적인 프로그래밍 규칙은 경우에 따라 달라집니다. 그 경우란, 바로 C++의 어떤 부분을 사용하느냐 입니다.
+		C++ 은 다음 네 가지 부분으로 나누어 생각할 수 있습니다.
+
+		1. C언어에서 물려받은 부분
+		2. 객체 지향 개념의 C++
+		3. 템플릿 C++
+		4. STL
+	*/
+}
