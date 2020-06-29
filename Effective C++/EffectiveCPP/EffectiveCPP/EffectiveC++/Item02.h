14e905cf80cc3fd19f60b44dd7bc8fdc58c7de21
wlsvy <hubjpkim@gmail.com>
2020-06-29 23:07:29 +0900

    Update Effective C++ Item02

---
+++ Effective C++/EffectiveCPP/EffectiveCPP/EffectiveC++/Item02.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <array>
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+//#define 을 쓰려거든 const, enum, inline 을 떠올리자
+namespace Item02 {
+	using namespace std;
+
+	class CostEstimate {
+	private:
+		static const double FudgeFactor; //정적 클래스 상수 선언 -> 헤더 파일에 둔다
+	};
+	//const double FudgeFactor = 1.5; //정적 클래스 상수의 정의 -> 구현 파일에 둔다.
+
+	//enum을 이용한 상수 구현
+	class GamePlayer {
+	private:
+		enum {NumTurns = 5};
+
+		int scores[NumTurns];	//NumTurns 값은 컴파일 시간에 알 수 있다.
+	};
+
+
+	inline int Max(int lhs, int rhs) {
+		return lhs > rhs ? lhs : rhs;
+	}
+
+	inline void RunSample() {
+		int a = 5;
+		int b = 0;
+
+		MAX(a++, b);	//a 2번 증가
+
+		cout << a << " " << b << endl;
+
+		Max(a++, b);	//이상 없음
+
+		cout << a << " " << b << endl;
+	}
+}
--- Effective C++/EffectiveCPP/EffectiveCPP/EffectiveCPP.cpp
+++ Effective C++/EffectiveCPP/EffectiveCPP/EffectiveCPP.cpp
@@ -8,55 +8,14 @@
 #include <map>
 #include <algorithm>
 #include <math.h>
+
+#include "EffectiveC++/Item02.h"
+
 using namespace std;
 
-struct inputtag {};
-struct outputtag {};
-struct forwardtag {};
-
-class it {
-public:
-	using iType = inputtag;
-};
-
-class it1 {
-public:
-	using iType = outputtag;
-};
-
-class it2 {
-public:
-	using iType = forwardtag;
-};
-
-template<typename T>
-void foo(T val, inputtag) { cout << "inputtag" << endl; }
-
-template<typename T>
-void foo(T val, outputtag) { cout << "outputtag" << endl; }
-
-template<typename T>
-void foo(T val, forwardtag) { cout << "forwardtag" << endl; }
-
-template<typename T>
-struct traits {
-	typedef typename T::iType iType;
-};
-
-template<typename T>
-void foo(T val) {
-	foo(val, typename T::iType());
-}
 
 int main()
 {
-	it i1;
-	it1 i2;
-	it2 i3;
-	foo(i1);
-	foo(i2);
-	foo(i3);
-
-
+	Item02::RunSample();
 	return 0;
 }
