30d585fb2f196e21ec38dd358b994e3b7add58c9
Jinpyo Kim <hubjpkim@gmail.com>
2020-07-29 03:31:26 +0900

    Create WallChecking.cpp

---
+++ Coding_Problem_Solving/WallChecking.cpp
@@ -0,0 +1,49 @@
+vector<int> Weak;
+vector<int> Dist;
+int N;
+int answer = INT_MAX;
+
+vector<int> Weak;
+vector<int> Dist;
+int N;
+int answer = INT_MAX;
+
+void dfs(vector<bool> before, int cnt, int weakSize) {
+
+	if (cnt >= answer) return;
+	if (weakSize == 0) {
+		answer = min(answer, cnt);
+		return;
+	}
+	if (cnt >= Dist.size()) return;
+
+	for (auto w : Weak) {
+		if (!before[w]) continue;
+
+		auto after = before;
+		auto afterWeakSize = weakSize;
+		for (int i = w; i <= w + Dist[cnt]; i++) {
+			if (after[i % N]) {
+				after[i % N] = false;
+				afterWeakSize--;
+			}
+		}
+		dfs(move(after), cnt + 1, afterWeakSize);
+	}
+}
+
+int solution(int n, vector<int> weak, vector<int> dist) {
+	sort(dist.begin(), dist.end(), greater<int>());
+
+	N = n;
+	Weak = weak;
+	Dist = dist;
+
+	vector<bool> walls(N, false);
+	for_each(weak.begin(), weak.end(), [&walls](int i) {walls[i] = true; });
+
+	dfs(move(walls), 0, Weak.size());
+
+	if (answer == INT_MAX) return -1;
+	return answer;
+}
