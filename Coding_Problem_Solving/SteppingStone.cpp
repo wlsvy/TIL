6abb9fd729972c5e8d7df61eb916554505cdb128
Jinpyo Kim <hubjpkim@gmail.com>
2020-07-31 00:55:36 +0900

    Update SteppingStone.cpp

--- Coding_Problem_Solving/SteppingStone.cpp
+++ Coding_Problem_Solving/SteppingStone.cpp
@@ -1,3 +1,5 @@
+//첫번째 방법 : Map 활용
+
 #include <string>
 #include <vector>
 #include <iostream>
@@ -36,4 +38,55 @@ int solution(vector<int> stones, int k) {
 	}
 
 	return (--expired)->first;
+}
+
+//두번째 방법 : 이분 탐색 
+
+#include <string>
+#include <vector>
+using namespace std;
+bool isPossible(int n, const vector<int>& stones, int k) {
+    int currLength = 0;
+    for (int i = 0; i < stones.size(); i++) {
+        if (stones[i] - n <= 0)
+            currLength++;
+        else
+            currLength = 0;
+        if (currLength >= k)
+            return false;
+    }
+    return true;
+}
+int solution(vector<int> stones, int k) {
+    int lo = 1;
+    int hi = 200000000;
+    while (lo <= hi) {
+        int mid = (lo + hi) / 2;
+        if (isPossible(mid, stones, k))
+            lo = mid + 1;
+        else
+            hi = mid - 1;
+    }
+    return lo;
+}
+
+
+//세번째 방법 : multiset
+
+#include <bits/stdc++.h>
+using namespace std;
+multiset <int> s;
+
+int solution(vector<int> a,int k) {
+    int ans = 1e9;
+    for(int i = 0;i < k-1;i++) {
+       s.insert(a[i]);
+    }
+    for(int i = k-1;i < a.size();i++) {
+       s.insert(a[i]);
+       auto it = s.end(); --it;
+       ans = min(ans,*it);
+       s.erase(s.find(a[i-(k-1)]));
+    }
+    return ans;
 }
