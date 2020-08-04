768376979c7d296991c82c897cf8b1bc74e3f921
Jinpyo Kim <hubjpkim@gmail.com>
2020-08-04 21:05:42 +0900

    Update ThanksgivingDayTraffic.cpp

--- Coding_Problem_Solving/ThanksgivingDayTraffic.cpp
+++ Coding_Problem_Solving/ThanksgivingDayTraffic.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<float> vf[2];
+vector<float> start, finish;
 
 int solution(vector<string> lines) {
 	auto N = lines.size();
@@ -12,8 +12,8 @@ int solution(vector<string> lines) {
 		auto m = stof(line.substr(14, 2));
 		auto s = stof(line.substr(17, 6));
 		auto t = stof(line.substr(24, secPos - 24));
-		vf[0].push_back(h * 3600 + m * 60 + s - t + 0.001f);
-		vf[1].push_back(h * 3600 + m * 60 + s);
+		start.push_back(h * 3600 + m * 60 + s - t + 0.001f);
+		finish.push_back(h * 3600 + m * 60 + s);
 	}
 	
 	int answer = 0;
@@ -21,19 +21,19 @@ int solution(vector<string> lines) {
 	{
 		auto testTarget =
 		{
-			vf[0][i] - 0.999f, 
-			vf[0][i], 
-			vf[1][i] - 0.999f, 
-			vf[1][i]
+			start[i] - 0.999f, 
+			start[i], 
+			finish[i] - 0.999f,
+			finish[i]
 		};
 		for (auto t : testTarget) {
 			int tmp = 0;
 			for (int j = 0; j < N; j++) {
 
-				if ((t <= vf[0][j] && vf[0][j] < t + 1.0f) ||
-					(t <= vf[1][j] && vf[1][j] < t + 1.0f) ||
-					(t <= vf[0][j] && vf[1][j] < t + 1.0f) ||
-					(vf[0][j] <= t && t + 1.0f < vf[1][j]))
+				if ((t <= start[j] && start[j] < t + 1.0f) ||
+					(t <= finish[j] && finish[j] < t + 1.0f) ||
+					(t <= start[j] && finish[j] < t + 1.0f) ||
+					(start[j] <= t && t + 1.0f <= finish[j]))
 				{
 					tmp++;
 				}
