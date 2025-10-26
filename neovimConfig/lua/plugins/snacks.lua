e0e587486757d549d5c4f5d142b3bdfc022d9d38
wlsvy <hubjpkim@gmail.com>
2025-10-26 23:35:26 +0900

    update neovimconfig

--- neovimConfig/lua/plugins/snacks.lua
+++ neovimConfig/lua/plugins/snacks.lua
@@ -19,30 +19,7 @@ return {
         statuscolumn = { enabled = true },
         words = { enabled = true },
         lazygit = { },
-        image = {
-            doc = {
-                -- enable image viewer for documents
-                -- a treesitter parser must be available for the enabled languages.
-                enabled = true,
-                -- render the image inline in the buffer
-                -- if your env doesn't support unicode placeholders, this will be disabled
-                -- takes precedence over `opts.float` on supported terminals
-                inline = true,
-                -- render the image in a floating window
-                -- only used if `opts.inline` is disabled
-                float = false,
-                max_width = 80,
-                max_height = 40,
-                -- Set to `true`, to conceal the image text when rendering inline.
-                -- (experimental)
-                ---@param lang string tree-sitter language
-                ---@param type snacks.image.Type image type
-                conceal = function(lang, type)
-                    -- only conceal math expressions
-                    return type == "math"
-                end,
-            },
-        },
+        image = { enabled = false, },
     },
     priority=1000,
 
