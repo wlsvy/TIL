fd1a9c853b904f783e040cf1aabfed2d30c30a6f
wlsvy <hubjpkim@gmail.com>
2026-03-31 23:31:13 +0900

    update neovimConfig

--- neovimConfig/lua/plugins/treesitter.lua
+++ neovimConfig/lua/plugins/treesitter.lua
@@ -2,8 +2,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
     dependencies = { "OXY2DEV/markview.nvim",},
-    lazy = false,
-    build = ':TSUpdate',
     opts = {
         ensure_installed = { "c", "lua", "markdown", "markdown_inline", "query", "vim", "vimdoc", "c_sharp" ,"python" },
         indent = { enable = true },
