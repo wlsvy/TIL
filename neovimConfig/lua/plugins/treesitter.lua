3cc23636435ded7c96a152d2b4ec6b362e2e9d04
김진표 [jinpyo.kim] <jinpyo.kim@devcat.com>
2026-03-31 15:21:22 +0900

    update neovimconfig

--- neovimConfig/lua/plugins/treesitter.lua
+++ neovimConfig/lua/plugins/treesitter.lua
@@ -2,9 +2,12 @@ return {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
     dependencies = { "OXY2DEV/markview.nvim",},
-    opts = { 
+    lazy = false,
+    build = ':TSUpdate',
+    opts = {
         ensure_installed = { "c", "lua", "markdown", "markdown_inline", "query", "vim", "vimdoc", "c_sharp" ,"python" },
         indent = { enable = true },
         folding = { enable = true, },
     },
+    
 }
